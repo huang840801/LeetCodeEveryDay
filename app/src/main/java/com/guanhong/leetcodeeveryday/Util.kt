6a772ce7dc12ccb1dd941cc0874128bfe37a6d61
GuanHongHuang <david840801@gmail.com>
2020-01-23 23:33:14 +0800

    121 maxProfit

---
+++ app/src/main/java/com/guanhong/leetcodeeveryday/Util.kt
@@ -0,0 +1,34 @@
+package com.guanhong.leetcodeeveryday
+
+class Util {
+
+    private object Holder {
+
+        val INSTANCE = Util()
+    }
+
+    companion object{
+
+        val instance by lazy { Holder.INSTANCE }
+    }
+
+    fun getMax(intArray : IntArray) : Int {
+
+        if (intArray.count() == 0) {
+
+            return -99
+        }
+
+        var answer = intArray[0]
+
+        for (num in intArray) {
+
+            if (num > answer) {
+
+                answer = num
+            }
+        }
+
+        return answer
+    }
+}
---
+++ app/src/test/java/com/guanhong/leetcodeeveryday/LeetCode121Test.kt
@@ -0,0 +1,80 @@
+package com.guanhong.leetcodeeveryday
+
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class LeetCode121Test {
+
+    private lateinit var leetCode121 : LeetCode121
+
+    @Before
+    fun setUp() {
+
+        leetCode121 = LeetCode121()
+    }
+
+    @Test
+    fun testMaxProfit1() {
+
+        val actual = leetCode121.maxProfit(intArrayOf(7, 3, 1, 5, 2))
+        val expected = 4
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testMaxProfit2() {
+
+        val actual = leetCode121.maxProfit(intArrayOf(3, 1, 5, 2))
+        val expected = 4
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testMaxProfit3() {
+
+        val actual = leetCode121.maxProfit(intArrayOf(1, 5, 2))
+        val expected = 4
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testMaxProfit4() {
+
+        val actual = leetCode121.maxProfit(intArrayOf(1))
+        val expected = 0
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testMaxProfit5() {
+
+        val actual = leetCode121.maxProfit(intArrayOf(1, 2, 3))
+        val expected = 2
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testMaxProfit6() {
+
+        val actual = leetCode121.maxProfit(intArrayOf(3, 2, 1))
+        val expected = 0
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testMaxProfit7() {
+
+        val actual = leetCode121.maxProfit(intArrayOf(7, 6, 4, 3, 1))
+        val expected = 0
+
+        assertEquals(expected, actual)
+    }
+}
