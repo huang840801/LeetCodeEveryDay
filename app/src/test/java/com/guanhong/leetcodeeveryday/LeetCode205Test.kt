6123f0723a1eea4cdf2a5f7ca9f660d2b9aa3d0c
GuanHongHuang <david840801@gmail.com>
2020-01-27 20:36:46 +0800

    205 isIsomorphic

---
+++ app/src/test/java/com/guanhong/leetcodeeveryday/LeetCode205Test.kt
@@ -0,0 +1,71 @@
+package com.guanhong.leetcodeeveryday
+
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class LeetCode205Test {
+
+    private lateinit var leetCode205 : LeetCode205
+
+    @Before
+    fun setUp() {
+
+        leetCode205 = LeetCode205()
+    }
+
+    @Test
+    fun testIsIsomorphic1() {
+
+        val actual = leetCode205.isIsomorphic("paper", "title")
+        val expected = true
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testIsIsomorphic2() {
+
+        val actual = leetCode205.isIsomorphic("egg", "add")
+        val expected = true
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testIsIsomorphic3() {
+
+        val actual = leetCode205.isIsomorphic("egg", "tit")
+        val expected = false
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testIsIsomorphic4() {
+
+        val actual = leetCode205.isIsomorphic("foo", "bar")
+        val expected = false
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testIsIsomorphic5() {
+
+        val actual = leetCode205.isIsomorphic("", "")
+        val expected = true
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testIsIsomorphic6() {
+
+        val actual = leetCode205.isIsomorphic("ab", "aa")
+        val expected = false
+
+        assertEquals(expected, actual)
+    }
+}
