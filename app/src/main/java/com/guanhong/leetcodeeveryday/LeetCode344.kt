a75e252c5f5743d079c251914f712a79f9046d6c
GuanHongHuang <david840801@gmail.com>
2020-01-30 19:34:34 +0800

    344 reverseString

---
+++ app/src/main/java/com/guanhong/leetcodeeveryday/LeetCode344.kt
@@ -0,0 +1,19 @@
+package com.guanhong.leetcodeeveryday
+
+class LeetCode344 {
+
+    fun reverseString(s : CharArray) {
+
+        val size = s.count()
+        if (size == 0) return
+
+        var temp : Char
+
+        for (index in 0..((size - 1) / 2)) {
+
+            temp = s[index]
+            s[index] = s[size - index - 1]
+            s[size - index - 1] = temp
+        }
+    }
+}
