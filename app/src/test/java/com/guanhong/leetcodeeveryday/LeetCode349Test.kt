cd3b90cd5b7bc6d69cd8cdf4be3a538f65f3f9e4
GuanHongHuang <david840801@gmail.com>
2020-01-31 23:02:11 +0800

    349 intersection

---
+++ app/src/test/java/com/guanhong/leetcodeeveryday/LeetCode349Test.kt
@@ -0,0 +1,44 @@
+package com.guanhong.leetcodeeveryday
+
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class LeetCode349Test {
+
+    private lateinit var leetCode349 : LeetCode349
+
+    @Before
+    fun setUp() {
+
+        leetCode349 = LeetCode349()
+    }
+
+    @Test
+    fun testIntersection1() {
+
+        val actual = leetCode349.intersection(intArrayOf(1, 2, 2, 1), intArrayOf(2, 2))
+        val expected = intArrayOf(2)
+
+        assertArrayEquals(expected, actual)
+    }
+
+    @Test
+    fun testIntersection2() {
+
+        val actual = leetCode349.intersection(intArrayOf(4, 9, 5), intArrayOf(9, 4, 9, 8, 4))
+        val expected = intArrayOf(9, 4)
+
+        assertArrayEquals(expected, actual)
+    }
+
+    @Test
+    fun testIntersection3() {
+
+        val actual = leetCode349.intersection(intArrayOf(99), intArrayOf(9, 4, 9, 8, 4))
+        val expected = intArrayOf()
+
+        assertArrayEquals(expected, actual)
+    }
+}
