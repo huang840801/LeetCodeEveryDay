ca48e67565d24c1423fced77c06b4e18ac0c0b86
GuanHongHuang <david840801@gmail.com>
2020-02-08 13:26:28 +0800

    refactor 168

--- app/src/main/java/com/guanhong/leetcodeeveryday/LeetCode168.kt
+++ app/src/main/java/com/guanhong/leetcodeeveryday/LeetCode168.kt
@@ -4,62 +4,53 @@ class LeetCode168 {
 
     fun convertToTitle(n : Int) : String {
 
-        return if (n <= 0) ""
-        else resursion(n, "")
+        return resursion(n, "")
     }
 
-    // n >= 0
     fun resursion(n : Int, s : String) : String {
 
         var temp = s
 
         return if (n < 27) {
 
-            transTitle(n) + s
+            transTitle((n - 1)) + s
         } else {
 
-            temp = transTitle(n % 26) + temp
+            temp = transTitle((n - 1) % 26) + temp
 
-            if (n % 26 == 0) {
-
-                resursion((n / 26 - 1), temp)
-            } else {
-
-                resursion((n / 26), temp)
-            }
+            resursion((n - 1) / 26, temp)
         }
     }
 
     fun transTitle(n : Int) : String {
 
         return when (n) {
-            1 -> "A"
-            2 -> "B"
-            3 -> "C"
-            4 -> "D"
-            5 -> "E"
-            6 -> "F"
-            7 -> "G"
-            8 -> "H"
-            9 -> "I"
-            10 -> "J"
-            11 -> "K"
-            12 -> "L"
-            13 -> "M"
-            14 -> "N"
-            15 -> "O"
-            16 -> "P"
-            17 -> "Q"
-            18 -> "R"
-            19 -> "S"
-            20 -> "T"
-            21 -> "U"
-            22 -> "V"
-            23 -> "W"
-            24 -> "X"
-            25 -> "Y"
-            26 -> "Z"
-            0 -> "Z"
+            0 -> "A"
+            1 -> "B"
+            2 -> "C"
+            3 -> "D"
+            4 -> "E"
+            5 -> "F"
+            6 -> "G"
+            7 -> "H"
+            8 -> "I"
+            9 -> "J"
+            10 -> "K"
+            11 -> "L"
+            12 -> "M"
+            13 -> "N"
+            14 -> "O"
+            15 -> "P"
+            16 -> "Q"
+            17 -> "R"
+            18 -> "S"
+            19 -> "T"
+            20 -> "U"
+            21 -> "V"
+            22 -> "W"
+            23 -> "X"
+            24 -> "Y"
+            25 -> "Z"
             else -> ""
         }
     }
