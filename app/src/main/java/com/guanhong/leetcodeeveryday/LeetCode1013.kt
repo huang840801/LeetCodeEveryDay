110ff67b7b0653a844a4cbd89e41cfaf722df280
GuanHong <david840801@gmail.com>
2020-02-21 21:48:08 +0800

    1013 canThreePartsEqualSum

---
+++ app/src/main/java/com/guanhong/leetcodeeveryday/LeetCode1013.kt
@@ -0,0 +1,45 @@
+package com.guanhong.leetcodeeveryday
+
+class LeetCode1013 {
+
+    fun canThreePartsEqualSum(A : IntArray) : Boolean {
+
+        var firstTotal = 0
+        var secondTotal = 0
+        var thirdTotal = 0
+
+        var total = 0
+        val target : Int
+
+        A.forEach { i ->
+
+            total += i
+        }
+
+        if (total % 3 != 0) return false
+
+        target = total / 3
+
+        A.forEach { num ->
+
+            if (firstTotal != target) {
+
+                firstTotal += num
+            } else {
+
+                if (secondTotal != target) {
+
+                    secondTotal += num
+                } else {
+
+                    if (thirdTotal != target) {
+
+                        thirdTotal += num
+                    }
+                }
+            }
+        }
+
+        return (firstTotal == secondTotal && secondTotal == thirdTotal)
+    }
+}
