54d8af772b22bda1b5977e30d478c09fb282c2d8
GuanHong <david840801@gmail.com>
2020-03-02 21:32:39 +0800

    506 findRelativeRanks

---
+++ app/src/main/java/com/guanhong/leetcodeeveryday/LeetCode506.kt
@@ -0,0 +1,26 @@
+package com.guanhong.leetcodeeveryday
+
+class LeetCode506 {
+
+    fun findRelativeRanks(nums : IntArray) : Array<String> {
+
+        val sortedArray = nums.sortedBy { -it }
+
+        val map = mutableMapOf<Int, Int>()
+
+        sortedArray.forEachIndexed { index, i ->
+
+            map[i] = index
+        }
+
+        return nums.map {
+
+            when (map[it]) {
+                0 -> "Gold Medal"
+                1 -> "Silver Medal"
+                2 -> "Bronze Medal"
+                else -> "${map[it]}"
+            }
+        }.toTypedArray()
+    }
+}
