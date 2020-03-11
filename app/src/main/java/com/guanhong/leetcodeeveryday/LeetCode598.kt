764dedf19fa897bfff87a22d009187a135e1c4d9
GuanHong <david840801@gmail.com>
2020-03-11 21:49:37 +0800

    598 maxCount

---
+++ app/src/main/java/com/guanhong/leetcodeeveryday/LeetCode598.kt
@@ -0,0 +1,24 @@
+package com.guanhong.leetcodeeveryday
+
+class LeetCode598 {
+
+    fun maxCount(m : Int, n : Int, ops : Array<IntArray>) : Int {
+
+        var sameRow = m
+        var sameColumn = n
+
+        ops.forEach { intArray ->
+
+            if (intArray[0] < sameRow) {
+
+                sameRow = intArray[0]
+            }
+            if (intArray[1] < sameColumn) {
+
+                sameColumn = intArray[1]
+            }
+        }
+
+        return sameRow * sameColumn
+    }
+}
---
+++ app/src/test/java/com/guanhong/leetcodeeveryday/LeetCode598Test.kt
@@ -0,0 +1,37 @@
+package com.guanhong.leetcodeeveryday
+
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class LeetCode598Test {
+
+    private lateinit var leetCode : LeetCode598
+
+    @Before
+    fun setUp() {
+
+        leetCode = LeetCode598()
+    }
+
+    @Test
+    fun testMaxCount1() {
+
+        val actual = leetCode.maxCount(2, 1, arrayOf(intArrayOf(2, 2), intArrayOf(3, 3)))
+
+        val expected = 2
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testMaxCount2() {
+
+        val actual = leetCode.maxCount(3, 3, arrayOf(intArrayOf(2, 2), intArrayOf(3, 3)))
+
+        val expected = 4
+
+        assertEquals(expected, actual)
+    }
+}
