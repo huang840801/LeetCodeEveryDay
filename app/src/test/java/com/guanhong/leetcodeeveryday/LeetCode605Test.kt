8274fab78ea2e7e6734c6c39402b5cc8223e9995
GuanHong <david840801@gmail.com>
2020-03-15 12:37:54 +0800

    605 canPlaceFlowers

---
+++ app/src/test/java/com/guanhong/leetcodeeveryday/LeetCode605Test.kt
@@ -0,0 +1,81 @@
+package com.guanhong.leetcodeeveryday
+
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class LeetCode605Test {
+
+    private lateinit var leetCode : LeetCode605
+
+    @Before
+    fun setUp() {
+
+        leetCode = LeetCode605()
+    }
+
+    @Test
+    fun testCanPlaceFlowers1() {
+
+        val actual = leetCode.canPlaceFlowers(intArrayOf(1, 0, 0, 0, 1), 1)
+
+        assertTrue(actual)
+    }
+
+    @Test
+    fun testCanPlaceFlowers2() {
+
+        val actual = leetCode.canPlaceFlowers(intArrayOf(1, 0, 0, 0, 1), 2)
+
+        assertFalse(actual)
+    }
+
+    @Test
+    fun testCanPlaceFlowers3() {
+
+        val actual = leetCode.canPlaceFlowers(intArrayOf(1, 0, 1), 1)
+
+        assertFalse(actual)
+    }
+
+    @Test
+    fun testCanPlaceFlowers4() {
+
+        val actual = leetCode.canPlaceFlowers(intArrayOf(0), 1)
+
+        assertTrue(actual)
+    }
+
+    @Test
+    fun testCanPlaceFlowers5() {
+
+        val actual = leetCode.canPlaceFlowers(intArrayOf(0), 0)
+
+        assertTrue(actual)
+    }
+
+    @Test
+    fun testCanPlaceFlowers6() {
+
+        val actual = leetCode.canPlaceFlowers(intArrayOf(1), 1)
+
+        assertFalse(actual)
+    }
+
+    @Test
+    fun testCanPlaceFlowers7() {
+
+        val actual = leetCode.canPlaceFlowers(intArrayOf(1), 0)
+
+        assertTrue(actual)
+    }
+
+    @Test
+    fun testCanPlaceFlowers8() {
+
+        val actual = leetCode.canPlaceFlowers(intArrayOf(0, 0, 0), 2)
+
+        assertTrue(actual)
+    }
+}
