134dd0fd314475c14559c170e6aa3e9da9358d8c
GuanHong <david840801@gmail.com>
2020-03-19 20:55:22 +0800

    686 repeatedStringMatch

---
+++ app/src/main/java/com/guanhong/leetcodeeveryday/LeetCode686.kt
@@ -0,0 +1,22 @@
+package com.guanhong.leetcodeeveryday
+
+import java.lang.StringBuilder
+
+class LeetCode686 {
+
+    fun repeatedStringMatch(A : String, B : String) : Int {
+
+        val stringBuilder = StringBuilder()
+
+        for (index in 0..(B.length / A.length + 2)) {
+
+            if (stringBuilder.contains(B)) {
+
+                return index
+            }
+            stringBuilder.append(A)
+        }
+
+        return -1
+    }
+}
---
+++ app/src/test/java/com/guanhong/leetcodeeveryday/LeetCode686Test.kt
@@ -0,0 +1,89 @@
+package com.guanhong.leetcodeeveryday
+
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class LeetCode686Test {
+
+    private lateinit var leetCode : LeetCode686
+
+    @Before
+    fun setUp() {
+
+        leetCode = LeetCode686()
+    }
+
+    @Test
+    fun testRepeatedStringMatch1() {
+
+        val actual = leetCode.repeatedStringMatch("abcd", "cdabcdab")
+        val expected = 3
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testRepeatedStringMatch2() {
+
+        val actual = leetCode.repeatedStringMatch("abc", "bca")
+        val expected = 2
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testRepeatedStringMatch3() {
+
+        val actual = leetCode.repeatedStringMatch("ab", "bca")
+        val expected = -1
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testRepeatedStringMatch4() {
+
+        val actual = leetCode.repeatedStringMatch("ab", "ba")
+        val expected = 2
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testRepeatedStringMatch5() {
+
+        val actual = leetCode.repeatedStringMatch("abcd", "dcba")
+        val expected = -1
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testRepeatedStringMatch6() {
+
+        val actual = leetCode.repeatedStringMatch("a", "a")
+        val expected = 1
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testRepeatedStringMatch7() {
+
+        val actual = leetCode.repeatedStringMatch("aaaaaaaaaaaaaaaaaaaaaab", "ba")
+        val expected = 2
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testRepeatedStringMatch8() {
+
+        val actual = leetCode.repeatedStringMatch("abcbc", "cabcbca")
+        val expected = 3
+
+        assertEquals(expected, actual)
+    }
+}
