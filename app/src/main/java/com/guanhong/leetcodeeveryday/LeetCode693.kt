7bb19fe413d1a981844569ca332553ea6684a81f
GuanHong <david840801@gmail.com>
2020-03-21 23:08:06 +0800

    693 hasAlternatingBits

---
+++ app/src/main/java/com/guanhong/leetcodeeveryday/LeetCode693.kt
@@ -0,0 +1,37 @@
+package com.guanhong.leetcodeeveryday
+
+class LeetCode693 {
+
+    fun hasAlternatingBits(n : Int) : Boolean {
+
+        if (n < 3) return true
+
+        val binary = decimalToBinary(n)
+
+        var previousElement = binary[0]
+
+        for (index in 1 until binary.length) {
+
+            if (binary[index] != previousElement) {
+
+                previousElement = binary[index]
+            } else {
+
+                return false
+            }
+        }
+
+        return true
+    }
+
+    fun decimalToBinary(n : Int) : String {
+
+        return if (n < 2) {
+
+            n.toString()
+        } else {
+
+            decimalToBinary(n / 2) + decimalToBinary(n % 2)
+        }
+    }
+}
---
+++ app/src/test/java/com/guanhong/leetcodeeveryday/LeetCode693Test.kt
@@ -0,0 +1,65 @@
+package com.guanhong.leetcodeeveryday
+
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class LeetCode693Test {
+
+    private lateinit var leetCode : LeetCode693
+
+    @Before
+    fun setUp() {
+
+        leetCode = LeetCode693()
+    }
+    @Test
+    fun testDecimalToBinary1() {
+
+        val actual = leetCode.decimalToBinary(3)
+        val expected = "11"
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testDecimalToBinary2() {
+
+        val actual = leetCode.decimalToBinary(4)
+        val expected = "100"
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testDecimalToBinary3() {
+
+        val actual = leetCode.decimalToBinary(5)
+        val expected = "101"
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testHasAlternatingBits1() {
+
+        val actual = leetCode.hasAlternatingBits(5)
+
+        assertTrue(actual)
+    }
+    @Test
+    fun testHasAlternatingBits2() {
+
+        val actual = leetCode.hasAlternatingBits(7)
+
+        assertFalse(actual)
+    }
+    @Test
+    fun testHasAlternatingBits3() {
+
+        val actual = leetCode.hasAlternatingBits(11)
+
+        assertFalse(actual)
+    }
+}
