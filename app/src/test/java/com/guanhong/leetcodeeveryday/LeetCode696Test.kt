dc52cd810e16f3933fba843630e4b4fc31728e19
GuanHong <david840801@gmail.com>
2020-03-23 21:56:07 +0800

    696 countBinarySubstrings

---
+++ app/src/test/java/com/guanhong/leetcodeeveryday/LeetCode696Test.kt
@@ -0,0 +1,44 @@
+package com.guanhong.leetcodeeveryday
+
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class LeetCode696Test {
+
+    private lateinit var leetCode : LeetCode696
+
+    @Before
+    fun setUp() {
+
+        leetCode = LeetCode696()
+    }
+
+    @Test
+    fun testCountBinarySubstrings1() {
+
+        val actual = leetCode.countBinarySubstrings("00110011")
+        val expected = 6
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testCountBinarySubstrings2() {
+
+        val actual = leetCode.countBinarySubstrings("10101")
+        val expected = 4
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testCountBinarySubstrings3() {
+
+        val actual = leetCode.countBinarySubstrings("0011")
+        val expected = 2
+
+        assertEquals(expected, actual)
+    }
+}
