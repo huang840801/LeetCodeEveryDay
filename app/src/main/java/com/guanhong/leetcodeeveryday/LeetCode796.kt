a079e101f8d316a4a6ba580a34ba2d2c8c4348a9
GuanHong <david840801@gmail.com>
2020-04-03 20:01:51 +0800

    796 rotateString

---
+++ app/src/main/java/com/guanhong/leetcodeeveryday/LeetCode796.kt
@@ -0,0 +1,30 @@
+package com.guanhong.leetcodeeveryday
+
+class LeetCode796 {
+
+    fun rotateString(A : String, B : String) : Boolean {
+
+        if (A.isEmpty() && B.isEmpty()) return true
+
+        var shiftTime = 0
+        var a = A
+        var temp : Char
+
+        while (shiftTime < a.length) {
+
+            if (a == B) {
+
+                return true
+            }
+
+            temp = a[0]
+
+            a = a.drop(1)
+            a += temp
+
+            shiftTime++
+        }
+
+        return false
+    }
+}
---
+++ app/src/test/java/com/guanhong/leetcodeeveryday/LeetCode796Test.kt
@@ -0,0 +1,41 @@
+package com.guanhong.leetcodeeveryday
+
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class LeetCode796Test {
+
+    private lateinit var leetCode : LeetCode796
+
+    @Before
+    fun setUp() {
+
+        leetCode = LeetCode796()
+    }
+
+    @Test
+    fun testRotateString1() {
+
+        val actual = leetCode.rotateString("abcde", "cdeab")
+
+        assertTrue(actual)
+    }
+
+    @Test
+    fun testRotateString2() {
+
+        val actual = leetCode.rotateString("abcde", "abced")
+
+        assertFalse(actual)
+    }
+
+    @Test
+    fun testRotateString3() {
+
+        val actual = leetCode.rotateString("abac", "acba")
+
+        assertFalse(actual)
+    }
+}
