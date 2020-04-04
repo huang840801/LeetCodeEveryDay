4f843f8bd0207430620101d29fa4f2ee7ccef0cd
GuanHong <david840801@gmail.com>
2020-04-04 22:02:45 +0800

    832 flipAndInvertImage

---
+++ app/src/test/java/com/guanhong/leetcodeeveryday/LeetCode832Test.kt
@@ -0,0 +1,69 @@
+package com.guanhong.leetcodeeveryday
+
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class LeetCode832Test {
+
+    private lateinit var leetCode : LeetCode832
+
+    @Before
+    fun setUp() {
+
+        leetCode = LeetCode832()
+    }
+
+    @Test
+    fun testFlipAndInvertImage1() {
+
+        val actual = leetCode.flipAndInvertImage(
+            arrayOf(
+                intArrayOf(1, 1, 0),
+                intArrayOf(1, 0, 1),
+                intArrayOf(0, 0, 0)
+            )
+        )
+
+        val expected = arrayOf(
+            intArrayOf(1, 0, 0),
+            intArrayOf(0, 1, 0),
+            intArrayOf(1, 1, 1)
+        )
+
+        assertArrayEquals(expected, actual)
+    }
+
+    @Test
+    fun testFlipAndInvertImage2() {
+
+        val actual = leetCode.flipAndInvertImage(
+            arrayOf(
+                intArrayOf(1, 1, 0, 0),
+                intArrayOf(1, 0, 0, 1),
+                intArrayOf(0, 1, 1, 1),
+                intArrayOf(1, 0, 1, 0)
+            )
+        )
+
+        val expected = arrayOf(
+            intArrayOf(1, 1, 0, 0),
+            intArrayOf(0, 1, 1, 0),
+            intArrayOf(0, 0, 0, 1),
+            intArrayOf(1, 0, 1, 0)
+        )
+
+        assertArrayEquals(expected, actual)
+    }
+
+    @Test
+    fun testFlipAndInvertImage3() {
+
+        val actual = leetCode.flipAndInvertImage(arrayOf())
+
+        val expected = arrayOf<IntArray>()
+
+        assertArrayEquals(expected, actual)
+    }
+}
