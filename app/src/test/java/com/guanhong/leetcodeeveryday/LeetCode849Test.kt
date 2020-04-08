57401d04e6ed0129203cd70e87fc53ca2c5fb5ed
GuanHong <david840801@gmail.com>
2020-04-08 21:29:47 +0800

    849 maxDistToClosest

---
+++ app/src/test/java/com/guanhong/leetcodeeveryday/LeetCode849Test.kt
@@ -0,0 +1,49 @@
+package com.guanhong.leetcodeeveryday
+
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class LeetCode849Test {
+
+    private lateinit var leetCode : LeetCode849
+
+    @Before
+    fun setUp() {
+
+        leetCode = LeetCode849()
+    }
+
+    @Test
+    fun testMaxDistToClosest1() {
+
+        val actual = leetCode.maxDistToClosest(intArrayOf(1, 0, 0, 0, 1, 0, 1))
+
+        assertEquals(2, actual)
+    }
+
+    @Test
+    fun testMaxDistToClosest2() {
+
+        val actual = leetCode.maxDistToClosest(intArrayOf(1, 0, 0, 0))
+
+        assertEquals(3, actual)
+    }
+
+    @Test
+    fun testMaxDistToClosest3() {
+
+        val actual = leetCode.maxDistToClosest(intArrayOf(0, 0, 0, 0, 1))
+
+        assertEquals(4, actual)
+    }
+
+    @Test
+    fun testMaxDistToClosest4() {
+
+        val actual = leetCode.maxDistToClosest(intArrayOf(0, 0, 0, 0, 1, 0, 0))
+
+        assertEquals(4, actual)
+    }
+}
