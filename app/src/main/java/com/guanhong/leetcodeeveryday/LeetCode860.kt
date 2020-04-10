d3182cb6a28679f4e6506fd3d1033b030cb10684
GuanHong <david840801@gmail.com>
2020-04-10 21:56:14 +0800

    860 lemonadeChange

---
+++ app/src/main/java/com/guanhong/leetcodeeveryday/LeetCode860.kt
@@ -0,0 +1,48 @@
+package com.guanhong.leetcodeeveryday
+
+class LeetCode860 {
+
+    fun lemonadeChange(bills : IntArray) : Boolean {
+
+        if (!bills.all { it == 5 || it == 10 || it == 20 }) return false
+
+        val mutableMap = mutableMapOf(5 to 0, 10 to 0, 20 to 0)
+
+        bills.forEach {
+
+            if (it == 5) {
+
+                mutableMap[5] = mutableMap[5]!! + 1
+            } else {
+
+                var bill = it
+
+                while (bill >= 25 && mutableMap[20]!! > 0) {
+
+                    bill -= 20
+                    mutableMap[20] = mutableMap[20]!! - 1
+                }
+                while (bill >= 15 && mutableMap[10]!! > 0) {
+
+                    bill -= 10
+                    mutableMap[10] = mutableMap[10]!! - 1
+                }
+                while (bill >= 10 && mutableMap[5]!! > 0) {
+
+                    bill -= 5
+                    mutableMap[5] = mutableMap[5]!! - 1
+                }
+
+                if (bill != 5) {
+
+                    return false
+                } else {
+
+                    mutableMap[it] = mutableMap[it]!! + 1
+                }
+            }
+        }
+
+        return true
+    }
+}
---
+++ app/src/test/java/com/guanhong/leetcodeeveryday/LeetCode860Test.kt
@@ -0,0 +1,57 @@
+package com.guanhong.leetcodeeveryday
+
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class LeetCode860Test {
+
+    private lateinit var leetCode : LeetCode860
+
+    @Before
+    fun setUp() {
+
+        leetCode = LeetCode860()
+    }
+
+    @Test
+    fun testLemonadeChange1() {
+
+        val actual = leetCode.lemonadeChange(intArrayOf(5, 5, 5, 10, 20))
+
+        assertTrue(actual)
+    }
+
+    @Test
+    fun testLemonadeChange2() {
+
+        val actual = leetCode.lemonadeChange(intArrayOf(5, 5, 10))
+
+        assertTrue(actual)
+    }
+
+    @Test
+    fun testLemonadeChange3() {
+
+        val actual = leetCode.lemonadeChange(intArrayOf(10, 10))
+
+        assertFalse(actual)
+    }
+
+    @Test
+    fun testLemonadeChange4() {
+
+        val actual = leetCode.lemonadeChange(intArrayOf(5, 5, 10, 10, 20))
+
+        assertFalse(actual)
+    }
+
+    @Test
+    fun testLemonadeChange5() {
+
+        val actual = leetCode.lemonadeChange(intArrayOf(6))
+
+        assertFalse(actual)
+    }
+}
