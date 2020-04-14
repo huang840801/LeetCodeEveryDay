b6fe938dc4e1599e10dbf6755f409d7890df00c4
GuanHong <david840801@gmail.com>
2020-04-14 20:19:21 +0800

    888 fairCandySwap

---
+++ app/src/test/java/com/guanhong/leetcodeeveryday/LeetCode888Test.kt
@@ -0,0 +1,56 @@
+package com.guanhong.leetcodeeveryday
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+class LeetCode888Test {
+
+    private lateinit var leetCode : LeetCode888
+
+    @Before
+    fun setUp() {
+
+        leetCode = LeetCode888()
+    }
+
+    @Test
+    fun testFairCandySwap1() {
+
+        val actual = leetCode.fairCandySwap(intArrayOf(1, 1), intArrayOf(2, 2))
+
+        assertArrayEquals(intArrayOf(1, 2), actual)
+    }
+
+    @Test
+    fun testFairCandySwap2() {
+
+        val actual = leetCode.fairCandySwap(intArrayOf(1, 2), intArrayOf(2, 3))
+
+        assertArrayEquals(intArrayOf(1, 2), actual)
+    }
+
+    @Test
+    fun testFairCandySwap3() {
+
+        val actual = leetCode.fairCandySwap(intArrayOf(2), intArrayOf(1, 3))
+
+        assertArrayEquals(intArrayOf(2, 3), actual)
+    }
+
+    @Test
+    fun testFairCandySwap4() {
+
+        val actual = leetCode.fairCandySwap(intArrayOf(1, 2, 5), intArrayOf(2, 4))
+
+        assertArrayEquals(intArrayOf(5, 4), actual)
+    }
+
+    @Test
+    fun testFairCandySwap5() {
+
+        val actual = leetCode.fairCandySwap(intArrayOf(1, 2), intArrayOf(1, 2))
+
+        assertArrayEquals(intArrayOf(1, 1), actual)
+    }
+}
