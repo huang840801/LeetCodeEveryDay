743904c11742f5b7af4ff048d4b4ba50d2de5ff1
GuanHong <david840801@gmail.com>
2020-04-17 20:22:39 +0800

    914 hasGroupsSizeX

---
+++ app/src/main/java/com/guanhong/leetcodeeveryday/LeetCode914.kt
@@ -0,0 +1,39 @@
+package com.guanhong.leetcodeeveryday
+
+class LeetCode914 {
+
+    fun hasGroupsSizeX(deck : IntArray) : Boolean {
+
+        val mutableMap = mutableMapOf<Int, Int>()
+
+        deck.forEach {
+
+            if (mutableMap[it] == null) {
+
+                mutableMap[it] = 1
+            } else {
+
+                mutableMap[it] = mutableMap[it]!! + 1
+            }
+        }
+
+        val mutableList = mutableListOf<Int>()
+
+        mutableMap.forEach {
+
+            mutableList.add(it.value)
+        }
+
+        val min = mutableList.min()!!
+
+        for (i in 2..min) {
+
+            if (mutableList.all { it % i == 0 }) {
+
+                return true
+            }
+        }
+
+        return false
+    }
+}
