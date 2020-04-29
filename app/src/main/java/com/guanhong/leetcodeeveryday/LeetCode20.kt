b0b6309ada66058998c2b469b3bfb7876d42b296
GuanHong <david840801@gmail.com>
2020-04-29 20:38:30 +0800

    refactor 20 isValid

--- app/src/main/java/com/guanhong/leetcodeeveryday/LeetCode20.kt
+++ app/src/main/java/com/guanhong/leetcodeeveryday/LeetCode20.kt
@@ -4,58 +4,17 @@ class LeetCode20 {
 
     fun isValid(s : String) : Boolean {
 
-        if (s.count() % 2 != 0) return false
-        if (s.isEmpty()) return true
+        var string = s
 
-        var newS = ""
-        var index = 0
+        while (string.contains("()")
+            || string.contains("[]")
+            || string.contains("{}")) {
 
-        var needToAddLastChar = true
-        var hasNextRound = false
-
-        while (index < s.count() - 1) {
-
-            val thisChar = s[index]
-            val nextChar = s[index + 1]
-
-            if (checkParentheses(thisChar, nextChar)) {
-
-                if (index == s.count() - 2) {
-                    needToAddLastChar = false
-                }
-                hasNextRound = true
-                index += 2
-            } else {
-
-                newS += thisChar
-                index++
-            }
+            string = string.replace("()", "")
+            string = string.replace("[]", "")
+            string = string.replace("{}", "")
         }
 
-        if (needToAddLastChar) {
-
-            newS += s[s.count() - 1]
-        }
-        if (newS == s) {
-
-            return false
-        }
-
-        return if (hasNextRound) {
-            isValid(newS)
-        } else {
-            true
-        }
-    }
-
-    fun checkParentheses(forward : Char, backward : Char) : Boolean {
-
-        return when (forward) {
-
-            '{' -> backward == '}'
-            '[' -> backward == ']'
-            '(' -> backward == ')'
-            else -> false
-        }
+        return string.isEmpty()
     }
 }
