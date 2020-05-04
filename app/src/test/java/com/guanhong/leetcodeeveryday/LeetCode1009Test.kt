d067317a5a286d13398ca45e6d3d7c0f9e00d8e7
GuanHong <david840801@gmail.com>
2020-05-04 21:51:01 +0800

    1009 bitwiseComplement

---
+++ app/src/test/java/com/guanhong/leetcodeeveryday/LeetCode1009Test.kt
@@ -0,0 +1,62 @@
+package com.guanhong.leetcodeeveryday
+
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class LeetCode1009Test {
+
+    private lateinit var leetCode : LeetCode1009
+
+    @Before
+    fun setUp() {
+
+        leetCode = LeetCode1009()
+    }
+
+    @Test
+    fun testBitwiseComplement1() {
+
+        val expected = 2
+        val actual = leetCode.bitwiseComplement(5)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testBitwiseComplement2() {
+
+        val expected = 0
+        val actual = leetCode.bitwiseComplement(7)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testBitwiseComplement3() {
+
+        val expected = 5
+        val actual = leetCode.bitwiseComplement(10)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testBitwiseComplement4() {
+
+        val expected = 15
+        val actual = leetCode.bitwiseComplement(16)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testBitwiseComplement5() {
+
+        val expected = 0
+        val actual = leetCode.bitwiseComplement(15)
+
+        assertEquals(expected, actual)
+    }
+}
