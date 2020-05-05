917b60794d853486a6f531d483d198b9c9221563
GuanHong <david840801@gmail.com>
2020-05-05 22:15:50 +0800

    1018 prefixesDivBy5

---
+++ app/src/test/java/com/guanhong/leetcodeeveryday/LeetCode1018Test.kt
@@ -0,0 +1,62 @@
+package com.guanhong.leetcodeeveryday
+
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class LeetCode1018Test {
+
+    private lateinit var leetCode : LeetCode1018
+
+    @Before
+    fun setUp() {
+
+        leetCode = LeetCode1018()
+    }
+
+    @Test
+    fun testPrefixesDivBy51() {
+
+        val actual = leetCode.prefixesDivBy5(intArrayOf(0, 1, 1))
+
+        val expected = booleanArrayOf(true, false, false)
+        assertArrayEquals(expected, actual)
+    }
+
+    @Test
+    fun testPrefixesDivBy52() {
+
+        val actual = leetCode.prefixesDivBy5(intArrayOf(1, 1, 1))
+
+        val expected = booleanArrayOf(false, false, false)
+        assertArrayEquals(expected, actual)
+    }
+
+    @Test
+    fun testPrefixesDivBy53() {
+
+        val actual = leetCode.prefixesDivBy5(intArrayOf(0, 1, 1, 1, 1, 1))
+
+        val expected = booleanArrayOf(true, false, false, false, true, false)
+        assertArrayEquals(expected, actual)
+    }
+
+    @Test
+    fun testPrefixesDivBy54() {
+
+        val actual = leetCode.prefixesDivBy5(intArrayOf(1, 1, 1, 0, 1))
+
+        val expected = booleanArrayOf(false, false, false, false, false)
+        assertArrayEquals(expected, actual)
+    }
+
+    @Test
+    fun testPrefixesDivBy55() {
+
+        val actual = leetCode.prefixesDivBy5(intArrayOf(0, 0, 0))
+
+        val expected = booleanArrayOf(true, true, true)
+        assertArrayEquals(expected, actual)
+    }
+}
