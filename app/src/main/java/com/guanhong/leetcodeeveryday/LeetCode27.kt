f51678505786634bcab1c8b0c39adc3d69be0bff
GuanHong <david840801@gmail.com>
2020-05-10 09:25:44 +0800

    27 removeElement

---
+++ app/src/main/java/com/guanhong/leetcodeeveryday/LeetCode27.kt
@@ -0,0 +1,21 @@
+package com.guanhong.leetcodeeveryday
+
+class LeetCode27 {
+
+    fun removeElement(nums : IntArray, `val` : Int) : Int {
+
+        var index = 0
+
+        nums.forEach {
+
+            if (it != `val`) {
+
+                nums[index] = it
+
+                index++
+            }
+        }
+
+        return index
+    }
+}
