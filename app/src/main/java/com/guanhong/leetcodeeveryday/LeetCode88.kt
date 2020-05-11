357601bfcccfa57da818b43cae52d9a62c6061d2
GuanHong <david840801@gmail.com>
2020-05-11 22:19:41 +0800

    88 merge

---
+++ app/src/main/java/com/guanhong/leetcodeeveryday/LeetCode88.kt
@@ -0,0 +1,25 @@
+package com.guanhong.leetcodeeveryday
+
+class LeetCode88 {
+
+    fun merge(nums1 : IntArray, m : Int, nums2 : IntArray, n : Int) {
+
+        val mutableList = mutableListOf<Int>()
+
+        for (index in 0 until m) {
+
+            mutableList.add(nums1[index])
+        }
+        for (index in 0 until n) {
+
+            mutableList.add(nums2[index])
+        }
+
+        val sortedList = mutableList.sorted()
+
+        for (index in 0 until nums1.count()) {
+
+            nums1[index] = sortedList[index]
+        }
+    }
+}
