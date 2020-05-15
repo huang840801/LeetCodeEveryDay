000e0ca89ae06ca1eeb9c00ed4c493bcace52f45
GuanHong <david840801@gmail.com>
2020-05-15 20:23:09 +0800

    111 minDepth

---
+++ app/src/main/java/com/guanhong/leetcodeeveryday/LeetCode111.kt
@@ -0,0 +1,15 @@
+package com.guanhong.leetcodeeveryday
+
+import kotlin.math.min
+
+class LeetCode111 {
+
+    fun minDepth(root : TreeNode?) : Int {
+
+        if (root == null) return 0
+        if (root.left == null) return minDepth(root.right) + 1
+        if (root.right == null) return minDepth(root.left) + 1
+
+        return min(minDepth(root.left), minDepth(root.right)) + 1
+    }
+}
