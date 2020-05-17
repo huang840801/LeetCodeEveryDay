3e0f9275260dbd14ab326860813bf9fbb40ac1de
GuanHong <david840801@gmail.com>
2020-05-17 13:13:19 +0800

    refactor 122

--- app/src/main/java/com/guanhong/leetcodeeveryday/LeetCode122.kt
+++ app/src/main/java/com/guanhong/leetcodeeveryday/LeetCode122.kt
@@ -4,68 +4,17 @@ class LeetCode122 {
 
     fun maxProfit(prices : IntArray) : Int {
 
-        if (prices.count() == 1) return 0
-
         var profit = 0
 
-        var listIndex = 0
+        for (index in 1..prices.lastIndex) {
 
-        val increaseCollectionList = mutableListOf<List<Int>>()
+            val thisPrice = prices[index]
+            val previousPrice = prices[index - 1]
 
-        val increaseList = mutableListOf<Int>()
+            if (thisPrice > previousPrice) {
 
-        var temp = prices[0]
-
-        var isIncrease = temp < prices[1]
-
-        if (isIncrease) {
-
-            increaseList.add(temp)
-        }
-
-        for (index in 1 until prices.count()) {
-
-            if (temp <= prices[index]) {
-
-                if (!isIncrease) {
-
-                    increaseList.add(temp)
-                    isIncrease = true
-                }
-
-                increaseList.add(prices[index])
-            } else {
-
-                if (isIncrease) {
-
-                    val listTemp = increaseList.map { it }
-
-                    increaseCollectionList.add(listTemp)
-
-                    increaseList.clear()
-
-                    listIndex++
-                }
-
-                isIncrease = false
+                profit += thisPrice - previousPrice
             }
-
-            temp = prices[index]
-        }
-
-        if (isIncrease) {
-
-            increaseCollectionList.add(increaseList)
-        }
-
-        increaseCollectionList.forEach {
-
-            val maxPrice = it[it.lastIndex]
-            val minPrice = it[0]
-
-            val thisProfit = maxPrice - minPrice
-
-            profit += thisProfit
         }
 
         return profit
