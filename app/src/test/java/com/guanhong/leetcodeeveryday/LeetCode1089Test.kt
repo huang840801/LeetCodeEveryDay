9096918e4c6b8ae4888a52ad329f5ca6fd7f3f8b
GuanHong <david840801@gmail.com>
2020-05-20 20:40:22 +0800

    1089 duplicateZeros

---
+++ app/src/test/java/com/guanhong/leetcodeeveryday/LeetCode1089Test.kt
@@ -0,0 +1,77 @@
+package com.guanhong.leetcodeeveryday
+
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class LeetCode1089Test {
+
+    private lateinit var leetCode : LeetCode1089
+
+    @Before
+    fun setUp() {
+
+        leetCode = LeetCode1089()
+    }
+
+    @Test
+    fun testDuplicateZeros1() {
+
+        val expected = intArrayOf(1, 0, 0, 2, 3, 0, 0, 4)
+        val actual = intArrayOf(1, 0, 2, 3, 0, 4, 5, 0)
+
+        leetCode.duplicateZeros(actual)
+        assertArrayEquals(expected, actual)
+    }
+
+    @Test
+    fun testDuplicateZeros2() {
+
+        val expected = intArrayOf(1, 2, 3)
+        val actual = intArrayOf(1, 2, 3)
+
+        leetCode.duplicateZeros(actual)
+        assertArrayEquals(expected, actual)
+    }
+
+    @Test
+    fun testDuplicateZeros3() {
+
+        val expected = intArrayOf(0, 0, 1)
+        val actual = intArrayOf(0, 1, 2)
+
+        leetCode.duplicateZeros(actual)
+        assertArrayEquals(expected, actual)
+    }
+
+    @Test
+    fun testDuplicateZeros4() {
+
+        val expected = intArrayOf(0, 0, 0, 0)
+        val actual = intArrayOf(0, 0, 1, 2)
+
+        leetCode.duplicateZeros(actual)
+        assertArrayEquals(expected, actual)
+    }
+
+    @Test
+    fun testDuplicateZeros5() {
+
+        val expected = intArrayOf(0, 0, 0, 0, 1)
+        val actual = intArrayOf(0, 0, 1, 2, 3)
+
+        leetCode.duplicateZeros(actual)
+        assertArrayEquals(expected, actual)
+    }
+
+    @Test
+    fun testDuplicateZeros6() {
+
+        val expected = intArrayOf(0, 0, 0, 0, 1, 2)
+        val actual = intArrayOf(0, 0, 1, 2, 3, 0)
+
+        leetCode.duplicateZeros(actual)
+        assertArrayEquals(expected, actual)
+    }
+}
