128eda847ead0bae922367b6f5feac306f65643e
GuanHong <david840801@gmail.com>
2020-05-27 21:01:46 +0800

    1170 numSmallerByFrequency

---
+++ app/src/main/java/com/guanhong/leetcodeeveryday/LeetCode1170.kt
@@ -0,0 +1,56 @@
+package com.guanhong.leetcodeeveryday
+
+class LeetCode1170 {
+
+    fun numSmallerByFrequency(queries : Array<String>, words : Array<String>) : IntArray {
+
+        val answer = IntArray(queries.count())
+
+        val queryFrequencyList = queries.map { getFrequency(it) }
+        val wordsFrequencyList = words.map { getFrequency(it) }
+
+        for (index in 0 until queries.count()) {
+
+            val queryFrequency = queryFrequencyList[index]
+
+            var sum = 0
+
+            wordsFrequencyList.forEach {
+
+                if (it > queryFrequency) {
+
+                    sum += 1
+                }
+            }
+
+            answer[index] = sum
+        }
+
+        return answer
+    }
+
+    fun getFrequency(string : String) : Int {
+
+        var frequency = 0
+
+        var smallerChar = 'z'
+
+        string.forEach {
+
+            if (smallerChar > it) {
+
+                smallerChar = it
+            }
+        }
+
+        string.forEach {
+
+            if (it == smallerChar) {
+
+                frequency += 1
+            }
+        }
+
+        return frequency
+    }
+}
