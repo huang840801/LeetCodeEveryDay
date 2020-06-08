65a5c7fdf5bb1e80f2409fd2fc88114994758153
GuanHong <david840801@gmail.com>
2020-06-08 20:02:16 +0800

    1295 findNumbers

---
+++ app/src/main/java/com/guanhong/leetcodeeveryday/LeetCode1295.kt
@@ -0,0 +1,27 @@
+package com.guanhong.leetcodeeveryday
+
+class LeetCode1295 {
+
+    fun findNumbers(nums : IntArray) : Int {
+
+        var answer = 0
+
+        nums.forEach {
+
+            if (getDigitNumber(it) % 2 == 0) answer++
+        }
+
+        return answer
+    }
+
+    private fun getDigitNumber(num : Int) : Int {
+
+        return if (num < 10) {
+
+            1
+        } else {
+
+            getDigitNumber((num / 10)) + 1
+        }
+    }
+}
