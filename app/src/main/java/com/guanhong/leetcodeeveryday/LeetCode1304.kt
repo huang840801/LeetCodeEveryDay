50fad5c5e8980537bff861c40e9244913c046af8
GuanHong <david840801@gmail.com>
2020-06-10 20:00:12 +0800

    1304 sumZero

---
+++ app/src/main/java/com/guanhong/leetcodeeveryday/LeetCode1304.kt
@@ -0,0 +1,24 @@
+package com.guanhong.leetcodeeveryday
+
+class LeetCode1304 {
+
+    fun sumZero(n : Int) : IntArray {
+
+        if (n == 2) return intArrayOf(-1, 1)
+
+        val answer = IntArray(n)
+
+        var sum = 0
+
+        for (index in 0 until n - 1) {
+
+            answer[index] = index
+
+            sum += index
+        }
+
+        answer[n - 1] = -sum
+
+        return answer
+    }
+}
