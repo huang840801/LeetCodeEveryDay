ef25cc57709634d19760c046d575e81826ae748e
GuanHong <david840801@gmail.com>
2020-06-15 20:20:53 +0800

    1323 refactor

--- app/src/main/java/com/guanhong/leetcodeeveryday/LeetCode1323.kt
+++ app/src/main/java/com/guanhong/leetcodeeveryday/LeetCode1323.kt
@@ -6,24 +6,6 @@ class LeetCode1323 {
 
     fun maximum69Number(num : Int) : Int {
 
-        val numToString = num.toString()
-
-        val stringBuilder = StringBuilder()
-
-        numToString.forEachIndexed { index, char ->
-
-            if (char == '6') {
-
-                stringBuilder.append("9")
-                stringBuilder.append(numToString.substring(index + 1 until numToString.count()))
-
-                return stringBuilder.toString().toInt()
-            } else {
-
-                stringBuilder.append("9")
-            }
-        }
-
-        return stringBuilder.toString().toInt()
+        return num.toString().replaceFirst("6", "9").toInt()
     }
 }
