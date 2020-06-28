11782392aa673654d64c904bde9acaf49452e6ce
GuanHong <david840801@gmail.com>
2020-06-28 16:43:13 +0800

    18 fourSum

---
+++ app/src/test/java/com/guanhong/leetcodeeveryday/LeetCode18Test.kt
@@ -0,0 +1,52 @@
+package com.guanhong.leetcodeeveryday
+
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class LeetCode18Test {
+
+    private lateinit var leetCode : LeetCode18
+
+    @Before
+    fun setUp() {
+
+        leetCode = LeetCode18()
+    }
+
+    @Test
+    fun testFourSum1() {
+
+        val expected = listOf(
+            listOf(-1, 0, 0, 1),
+            listOf(-2, -1, 1, 2),
+            listOf(-2, 0, 0, 2)
+        )
+        val actual = leetCode.fourSum(intArrayOf(1, 0, -1, 0, -2, 2), 0)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testFourSum2() {
+
+        val expected = listOf(
+            listOf(-3, -1, 0, 4)
+        )
+        val actual = leetCode.fourSum(intArrayOf(-3, -1, 0, 2, 4, 5), 0)
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testFourSum3() {
+
+        val expected = listOf(
+            listOf(-3, -1, 0, 4)
+        )
+        val actual = leetCode.fourSum(intArrayOf(-3, -1, 0, 4), 0)
+
+        assertEquals(expected, actual)
+    }
+}
