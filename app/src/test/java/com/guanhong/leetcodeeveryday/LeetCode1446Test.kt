8c9c4de18b28990d094586055310124742c00b1c
GuanHong <david840801@gmail.com>
2020-07-07 20:34:50 +0800

    1446 maxPower

---
+++ app/src/test/java/com/guanhong/leetcodeeveryday/LeetCode1446Test.kt
@@ -0,0 +1,89 @@
+package com.guanhong.leetcodeeveryday
+
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class LeetCode1446Test {
+
+    private lateinit var leetCode : LeetCode1446
+
+    @Before
+    fun setUp() {
+
+        leetCode = LeetCode1446()
+    }
+
+    @Test
+    fun testMaxPower1() {
+
+        val expected = 2
+        val actual = leetCode.maxPower("leetcode")
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testMaxPower2() {
+
+        val expected = 5
+        val actual = leetCode.maxPower("abbcccddddeeeeedcba")
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testMaxPower3() {
+
+        val expected = 5
+        val actual = leetCode.maxPower("triplepillooooow")
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testMaxPower4() {
+
+        val expected = 11
+        val actual = leetCode.maxPower("hooraaaaaaaaaaay")
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testMaxPower5() {
+
+        val expected = 1
+        val actual = leetCode.maxPower("tourist")
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testMaxPower6() {
+
+        val expected = 2
+        val actual = leetCode.maxPower("ttourist")
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testMaxPower7() {
+
+        val expected = 2
+        val actual = leetCode.maxPower("touristt")
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testMaxPower8() {
+
+        val expected = 1
+        val actual = leetCode.maxPower("r")
+
+        assertEquals(expected, actual)
+    }
+}
