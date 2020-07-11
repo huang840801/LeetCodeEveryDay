c3f0dae68e76cfb839f79824fb3a213ab440fb9b
GuanHong <david840801@gmail.com>
2020-07-11 11:55:57 +0800

    31 nextPermutation

---
+++ app/src/main/java/com/guanhong/leetcodeeveryday/LeetCode31.kt
@@ -0,0 +1,49 @@
+package com.guanhong.leetcodeeveryday
+
+class LeetCode31 {
+
+    fun nextPermutation(nums : IntArray) {
+
+        var previousNum : Int = nums.last()
+
+        for (index in nums.lastIndex - 1 downTo 0) {
+
+            val thisNum = nums[index]
+
+            if (thisNum < previousNum) {
+
+                //必為降冪排列
+                val sub = nums.toMutableList().subList(index + 1, nums.lastIndex + 1)
+
+                val numToBeChange = sub.lastOrNull { it > thisNum } ?: sub.last()
+
+                val numToBeChangeIndex = nums.lastIndexOf(numToBeChange)
+
+                swap(nums, index, numToBeChangeIndex)
+
+                sub.add(thisNum)
+                sub.remove(numToBeChange)
+
+                sub.sort()
+
+                for (index1 in 0 until sub.count()) {
+
+                    nums[index + 1 + index1] = sub[index1]
+                }
+
+                return
+            }
+
+            previousNum = thisNum
+        }
+
+        nums.reverse()
+    }
+
+    private fun swap(nums : IntArray, index : Int, index1 : Int) {
+
+        val temp = nums[index]
+        nums[index] = nums[index1]
+        nums[index1] = temp
+    }
+}
---
+++ app/src/test/java/com/guanhong/leetcodeeveryday/LeetCode31Test.kt
@@ -0,0 +1,116 @@
+package com.guanhong.leetcodeeveryday
+
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class LeetCode31Test {
+
+    private lateinit var leetCode : LeetCode31
+
+    @Before
+    fun setUp() {
+
+        leetCode = LeetCode31()
+    }
+
+    @Test
+    fun testNextPermutation1() {
+
+        val expected = intArrayOf(1, 2, 4, 3)
+
+        val array = intArrayOf(1, 2, 3, 4)
+
+        leetCode.nextPermutation(array)
+
+        val actual = array
+
+
+        assertArrayEquals(expected, actual)
+    }
+
+    @Test
+    fun testNextPermutation2() {
+
+        val expected = intArrayOf(1, 3, 2, 4)
+
+        val array = intArrayOf(1, 2, 4, 3)
+
+        leetCode.nextPermutation(array)
+
+        val actual = array
+
+        assertArrayEquals(expected, actual)
+    }
+
+    @Test
+    fun testNextPermutation3() {
+
+        val expected = intArrayOf(1, 2, 3)
+
+        val array = intArrayOf(3, 2, 1)
+
+        leetCode.nextPermutation(array)
+
+        val actual = array
+
+        assertArrayEquals(expected, actual)
+    }
+
+    @Test
+    fun testNextPermutation4() {
+
+        val expected = intArrayOf(1, 5, 1)
+
+        val array = intArrayOf(1, 1, 5)
+
+        leetCode.nextPermutation(array)
+
+        val actual = array
+
+        assertArrayEquals(expected, actual)
+    }
+
+    @Test
+    fun testNextPermutation5() {
+
+        val expected = intArrayOf(8, 3, 6)
+
+        val array = intArrayOf(6, 8, 3)
+
+        leetCode.nextPermutation(array)
+
+        val actual = array
+
+        assertArrayEquals(expected, actual)
+    }
+
+    @Test
+    fun testNextPermutation6() {
+
+        val expected = intArrayOf(1, 2, 3, 8, 3, 6)
+
+        val array = intArrayOf(1, 2, 3, 6, 8, 3)
+
+        leetCode.nextPermutation(array)
+
+        val actual = array
+
+        assertArrayEquals(expected, actual)
+    }
+
+    @Test
+    fun testNextPermutation7() {
+
+        val expected = intArrayOf(1, 2, 3, 8, 1, 2, 3, 6)
+
+        val array = intArrayOf(1, 2, 3, 6, 8, 3, 2, 1)
+
+        leetCode.nextPermutation(array)
+
+        val actual = array
+
+        assertArrayEquals(expected, actual)
+    }
+}
