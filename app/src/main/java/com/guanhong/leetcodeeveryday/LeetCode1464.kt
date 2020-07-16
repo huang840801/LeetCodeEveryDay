ca2a340cf4c8fd062d06fa660e11944af085ac31
GuanHong <david840801@gmail.com>
2020-07-16 20:57:00 +0800

    1464 maxProduct

---
+++ app/src/main/java/com/guanhong/leetcodeeveryday/LeetCode1464.kt
@@ -0,0 +1,19 @@
+package com.guanhong.leetcodeeveryday
+
+class LeetCode1464 {
+
+    fun maxProduct(nums : IntArray) : Int {
+
+        val maxIndex = nums.indexOf(nums.max()!!)
+
+        val max = nums[maxIndex]
+
+        nums[maxIndex] = 0
+
+        val secondMaxIndex = nums.indexOf(nums.max()!!)
+
+        val secondMax = nums[secondMaxIndex]
+
+        return (max - 1) * (secondMax - 1)
+    }
+}
