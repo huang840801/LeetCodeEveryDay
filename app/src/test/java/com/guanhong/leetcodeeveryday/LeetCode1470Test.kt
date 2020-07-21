2a3f8956c2c6c3680b6ded0f0f3e769eb2796356
GuanHong <david840801@gmail.com>
2020-07-21 20:29:02 +0800

    1470 shuffle

---
+++ app/src/test/java/com/guanhong/leetcodeeveryday/LeetCode1470Test.kt
@@ -0,0 +1,53 @@
+package com.guanhong.leetcodeeveryday
+
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class LeetCode1470Test {
+
+    private lateinit var leetCode : LeetCode1470
+
+    @Before
+    fun setUp() {
+
+        leetCode = LeetCode1470()
+    }
+
+    @Test
+    fun testShuffle1() {
+
+        val expected = intArrayOf(2, 3, 5, 4, 1, 7)
+        val actual = leetCode.shuffle(intArrayOf(2, 5, 1, 3, 4, 7), 3)
+
+        assertArrayEquals(expected, actual)
+    }
+
+    @Test
+    fun testShuffle2() {
+
+        val expected = intArrayOf(1, 4, 2, 3, 3, 2, 4, 1)
+        val actual = leetCode.shuffle(intArrayOf(1, 2, 3, 4, 4, 3, 2, 1), 4)
+
+        assertArrayEquals(expected, actual)
+    }
+
+    @Test
+    fun testShuffle3() {
+
+        val expected = intArrayOf(1, 2, 1, 2)
+        val actual = leetCode.shuffle(intArrayOf(1, 1, 2, 2), 2)
+
+        assertArrayEquals(expected, actual)
+    }
+
+    @Test
+    fun testShuffle4() {
+
+        val expected = intArrayOf(1, 2)
+        val actual = leetCode.shuffle(intArrayOf(1, 2), 1)
+
+        assertArrayEquals(expected, actual)
+    }
+}
