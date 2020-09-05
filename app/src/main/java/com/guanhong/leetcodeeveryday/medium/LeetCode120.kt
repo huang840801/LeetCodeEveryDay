fbbe0e2e7ee1ba38b44d72405e95468b4772fef5
GuanHong <david840801@gmail.com>
2020-09-05 12:33:43 +0800

    120 refactor

--- app/src/main/java/com/guanhong/leetcodeeveryday/medium/LeetCode120.kt
+++ app/src/main/java/com/guanhong/leetcodeeveryday/medium/LeetCode120.kt
@@ -6,27 +6,23 @@ class LeetCode120 {
 
     fun minimumTotal(triangle : List<List<Int>>) : Int {
 
-        val top = triangle[0][0]
+        val list : MutableList<MutableList<Int>> = mutableListOf()
 
-        if (triangle.count() == 1) return top
+        triangle.forEach { ints ->
 
-        return helper(triangle, 1, 0, top)
-    }
+            val temp = ints.map { it }.toMutableList()
 
-    fun helper(list : List<List<Int>>, row : Int, column : Int, sum : Int) : Int {
-
-        val left = list[row][column]
-        val right = list[row][column + 1]
-
-        return if (row == list.lastIndex) {
-
-            sum + min(left, right)
-        } else {
-
-            val leftSum = helper(list, row + 1, column, sum + left)
-            val rightSum = helper(list, row + 1, column + 1, sum + right)
-
-            min(leftSum, rightSum)
+            list.add(temp)
         }
+
+        for (i in triangle.size - 2 downTo 0) {
+
+            for (j in 0..i) {
+
+                list[i][j] = list[i][j] + min(list[i + 1][j], list[i + 1][j + 1])
+            }
+        }
+
+        return list[0][0]
     }
 }
